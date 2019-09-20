970b24f00153fce8a22c061f9447f604355ebacd
Anushka Pawar <apawar@usf.edu>
2019-09-19 20:10:42 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Deliverable_3_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+                { int size = 25;
+                int[] elements = new int[size];
+                for (int i = 0; 
+                    i <= 25; i++)
+                {
+                    elements[i] = i + 1;
+                    Console.Write(" vaule of element = " + elements[i]);
+
+                }
+            }
+            catch
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
